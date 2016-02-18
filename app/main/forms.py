0efd3a163ad40247da8d0b713ba1dcd7db141777
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2016-02-17 22:06:14 -0500

    Login/out working properly

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,6 +2,3 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
-class LoginForm(Form):
-    email = StringField('Email Address', validators=[Required()])
-    password = StringField('Password', validators=[Required()])
